8727c51fa3b2799ab97a8340a1712bcc0abe87a1
Farhat Jabeen <aroob4shoaib131@gmail.com>
2023-11-01 06:18:23 +0000

    data types

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 7770
+let accountEmail ="farhat@gmail.com"
+var accountPassword = "123456789"
+accountCity ="Jamshedpur";
+
+//accountId =361351
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
