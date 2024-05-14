776c1ab6bbcde5238a10d5793612d5eec2ffff19
Farhat Jabeen <aroob4shoaib131@gmail.com>
2024-05-14 09:34:55 +0000

    updated

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,2 +1,20 @@
 "use strict"; //Modern Js
-alert(3+3) // Not use node only in browser
+//alert(3+3) // Not use node only in browser
+
+let name = "Shoaib"
+let age = 30
+let isLoggedIn = false
+let state;
+
+//Data Types
+//number = 2 to the power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value //type - object
+//undefined =   //type - undefined
+//symbol = to find uniqueness of components in react 
+
+//object
+
+console.log(typeof(state));
