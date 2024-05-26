f0c35971924986b6abcd70a04607495100b34066
Farhat Jabeen <aroob4shoaib131@gmail.com>
2024-05-26 18:47:55 +0000

    operation done

--- 01_basic/04_operation.js
+++ 01_basic/04_operation.js
@@ -5,4 +5,31 @@ let num = 10
 let negNum = -num
 //console.log(negNum);
 
-console.log("Shoaib " + 5);
+// console.log("Shoaib " + 5);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+str1 = "Hello ";
+str2 = "Farhat"
+
+str3 = str1 + str2;
+//console.log(str3);  //Hello Farhat
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2");  //32
+// console.log(1 + "2" + 2);  //122
+
+console.log(+true);
+//console.log(true+); //error
+//console.log(+""); //0
+
+gameCounter = 100;
+gameCounter++; //101
+console.log(gameCounter);
+
